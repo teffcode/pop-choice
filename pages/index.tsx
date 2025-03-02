53008bb87db5946e963338e6b9fc611212e8f68a
teffcode <tefy.1@live.com>
2025-03-02 11:05:21 -0500

    feature(pop choice): add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,12 @@ import MovieRecommender from "@/components/MovieRecommender";
 
 export default function Home() {
   return (
-    <>
+    <section>
+      <h1 className="text-2xl md:text-4xl font-bold text-center text-gray-800 mb-8">
+        Pop Choice 🍿
+      </h1>
       <AddMovie />
       <MovieRecommender />
-    </>
+    </section>
   );
 }
